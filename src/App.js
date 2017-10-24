6f9e23848218937403274356e4d39428fdb674cc
mutt-n-head <lewis.isner@gmail.com>
2017-10-24 09:52:37 -0400

    Updates

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import './App.css';
 import EasyABC from './EasyABC';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Easy ABC</h1>
-        </header>
-        <EasyABC />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Easy ABC</h1>
+                </header>
+                <EasyABC />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/EasyABC.js
+++ src/EasyABC.js
@@ -1,25 +1,58 @@
 import React, {Component} from 'react';
+import Alphabets from './alphabets.json';
 
 class EasyABC extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            alphabets: Alphabets,
+            currentPosition: 0
+        };
+
+        this.playLetterSound = this.playLetterSound.bind(this);
+        this.handleNext = this.handleNext.bind(this);
+    }
+
+    playLetterSound(e) {
+        // playSound(this.state.alphabets[this.state.currentPosition].wordSound);
+    }
+
+    handleNext(e) {
+        this.setState(
+            {currentPosition: this.state.currentPosition + 1}
+        );
+    }
+
     render() {
         return (
             <div className="game">
                 <div className="options">
-                    <p>Some header text</p>
                     <div className="fields">
                         <div className="field-block">
-                            A
+                            {this.state.alphabets[this.state.currentPosition].letter}
                         </div>
                     </div>
                     <div className="buttons">
-                        <a href="#" className="button prev">Previous</a>
-                        <a href="#" className="button sound">Play Sound</a>
-                        <a href="#" className="button next">Next</a>
+                        <a className="button prev">Previous</a>
+                        <a onClick="this.playLetterSound" className="button sound">Play Sound</a>
+                        <a onClick={this.handleNext} className="button next">Next</a>
                     </div>
                     <div className="fields">
                         <div className="field-block">
-                            <div className="left-field">B</div>
-                            <div className="right-field">C</div>
+                            <div className="left-field">
+                                <div className="placeholder-span hide">Click next to view image</div>
+                                <img
+                                    className="letter-image"
+                                    src={this.state.alphabets[this.state.currentPosition].image}
+                                    alt={this.state.alphabets[this.state.currentPosition].word} />
+
+                            </div>
+                            <div className="right-field">
+                                <div className="placeholder-span hide">Click next to view word</div>
+                                <div className="word">
+                                    {this.state.alphabets[this.state.currentPosition].word.toUpperCase()}
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
